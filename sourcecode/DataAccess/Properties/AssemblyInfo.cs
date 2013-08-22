67ee1bc09b4eddf733ed6c09cd2c4dadb5e24035
num&letter <liefdiy@163.com>
2013-08-22 14:47:15 +0800

    modify solution

--- sourcecode/DataAccess/Properties/AssemblyInfo.cs
+++ sourcecode/DataAccess/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Mysoft.Platform.Component")]
+[assembly: AssemblyTitle("Mysoft.Platform.DataAccess")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mysoft.Platform.Component")]
+[assembly: AssemblyProduct("Mysoft.Platform.DataAccess")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("8e702888-5151-469d-adf7-f5a6f019e4c0")]
+[assembly: Guid("5a0e476e-c264-4fca-9101-47fd522c2e7f")]
 
 // 程序集的版本信息由下面四个值组成:
 //
--- sourcecode/WebSite/Properties/AssemblyInfo.cs
+++ sourcecode/WebSite/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebSite")]
+[assembly: AssemblyTitle("Mysoft.Platform.OnlineLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebSite")]
+[assembly: AssemblyProduct("Mysoft.Platform.OnlineLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
